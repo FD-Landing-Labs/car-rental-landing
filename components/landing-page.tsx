9cb2b7d34c094d5525b03d26566c9f3dfdd7b91b
Mohammedsasran <sasranmohammed8@gmail.com>
2026-01-19 19:23:26 +0530

    feat: added marqueebrandsection in landing page

--- components/landing-page.tsx
+++ components/landing-page.tsx
@@ -1,11 +1,13 @@
 import NavBar from "./nav-bar";
 import HeroSection from "./hero-section";
+import MarqueeBrand from "./marquee-brand";
+
 export default function LandingPage() {
-    return (
-        <div>
-           
-            <HeroSection />
-            
-        </div>
-    );
+  return (
+    <div>
+      <HeroSection />
+      <MarqueeBrand />
+      
+    </div>
+  );
 }
