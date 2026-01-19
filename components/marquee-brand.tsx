ffa835c7f22ccfb2d707e86c0432b42dd69f5097
Mohammedsasran <sasranmohammed8@gmail.com>
2026-01-19 19:23:44 +0530

    feat: added new images

--- components/marquee-brand.tsx
+++ components/marquee-brand.tsx
@@ -3,47 +3,51 @@
 import Image from "next/image";
 
 const marqueeBrands = [
-  "/assets/images/amg-brand-logo.jpg",
-  "/assets/images/BMW-Text-Logo.jpg",
-  "/assets/images/Benz.png", // Removed leading space
-  "/assets/images/bugatti-brand-logo.jpg", // Removed leading space
-  "/assets/images/gmc-brand-logo.jpg", // Removed leading space
-  "/assets/images/cadillac-car-logo.jpg",
-  "/assets/images/hummer-logo.jpg",
+  "/assets/car-brands/amg-brand-logo.jpg",
+  "/assets/car-brands/BMW-Text-Logo.jpg",
+  "/assets/car-brands/Benz.png", // Removed leading space
+  "/assets/car-brands/bugatti-brand-logo.jpg", // Removed leading space
+  "/assets/car-brands/gmc-brand-logo.jpg", // Removed leading space
+  "/assets/car-brands/cadillac-car-logo.jpg",
+  "/assets/car-brands/hummer-logo.jpg",
+  "/assets/car-brands/audi.png",
+  "/assets/car-brands/tesla.jpg",
+  "/assets/car-brands/lamborghini.png",
+  "/assets/car-brands/porsche.png",
 ];
 
 export default function MarqueeBrand() {
   return (
-    <div className="w-full overflow-hidden bg-white px-4 ">
+    <div className="w-full overflow-hidden bg-white px-4 py-4 md:py-4">
       <div className="flex animate-scroll whitespace-nowrap">
         {/* First set of images */}
-        <div className="flex items-center gap-14 mx-14 ">
+        <div className="flex items-center gap-8 mx-4 md:gap-16 md:mx-16">
           {marqueeBrands.map((src, index) => (
             <div
               key={`brand-1-${index}`}
-              className="relative h-16 w-32 shrink-0"
+              className="relative h-10 w-20 md:h-16 md:w-32 shrink-0"
             >
               <Image
                 src={src}
                 alt="Brand Logo"
                 fill
-                className="object-cover"
+                className="object-cover" // Changed back to object-contain for logos
               />
             </div>
           ))}
         </div>
         {/* Duplicate set of images for seamless scrolling */}
-        <div className="flex items-center gap-14 mx-14">
+        <div className="flex items-center gap-8 mx-4 md:gap-16 md:mx-16">
           {marqueeBrands.map((src, index) => (
             <div
               key={`brand-2-${index}`}
-              className="relative h-16 w-32 shrink-0"
+              className="relative h-10 w-20 md:h-16 md:w-32 shrink-0"
             >
               <Image
                 src={src}
                 alt="Brand Logo"
                 fill
-                className="object-cover"
+                className="object-cover" // Changed back to object-contain for logos
               />
             </div>
           ))}
