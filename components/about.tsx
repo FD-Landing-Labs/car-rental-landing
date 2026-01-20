956a68025363cae8a40981c7c7f01638bb0802b8
Mohammedsasran <sasranmohammed8@gmail.com>
2026-01-20 11:22:36 +0530

    feat: created about page

---
+++ components/about.tsx
@@ -0,0 +1,47 @@
+export default function About() {
+  const stats = [
+    { label: "Luxury Cars", value: "500+" },
+    { label: "Road Assistance", value: "24/7" },
+    { label: "Service Guarantee", value: "100%" },
+    { label: "Pickup Locations", value: "60+" },
+    { label: "Satisfied Clients", value: "800+" },
+  ];
+
+  return (
+    <section className="bg-white py-20 px-4 md:px-2 container mx-auto">
+      <div className="flex flex-col md:flex-row justify-between items-start gap-12 mb-24">
+        {/* Left Side - Headline */}
+        <div className="w-full md:w-1/2">
+          <h2 className="font-header text-5xl md:text-5xl leading-[1.1] text-black">
+            Drive Luxury  Live Freedom
+          </h2>
+        </div>
+
+        {/* Right Side - Description */}
+        <div className="flex flex-col w-full md:w-8/12  gap-4">
+          <p className=" text-black text-xl leading-relaxed font-medium">
+            Experience premium car rentalscrafted for comfort,perfomance, and style. 
+            Whether its a quick business trip or long <span className="text-gray-500">weekend getaway our fleet is 
+            designed to elevate your journey </span>  </p>
+
+            <div className="grid grid-cols-2 md:grid-cols-5 gap-2 border-t border-gray-300 pt-6">
+        {stats.map((stat, index) => (
+          <div key={index} className="flex flex-col  border-l border-gray-300 pl-4">
+            <span className=" text-4xl  text-black">
+              {stat.value}
+            </span>
+            <span className=" text-sm text-gray-500  font-medium tracking-wide">
+              {stat.label}
+            </span>
+          </div>
+        ))}
+      </div>
+
+        </div>
+      </div>
+
+      {/* Stats Row */}
+      
+    </section>
+  );
+}
