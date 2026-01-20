5558206ecc65452704bb54658ffd8bafd7ccd656
Mohammedsasran <sasranmohammed8@gmail.com>
2026-01-20 11:22:58 +0530

    feat: imported about page in landing page

--- components/landing-page.tsx
+++ components/landing-page.tsx
@@ -1,13 +1,14 @@
 import NavBar from "./nav-bar";
 import HeroSection from "./hero-section";
 import MarqueeBrand from "./marquee-brand";
+import About from "./about";
 
 export default function LandingPage() {
   return (
     <div>
       <HeroSection />
       <MarqueeBrand />
-      
+      <About />
     </div>
   );
 }
